c35c713b6ba9c430c4da3b044a0499f620790843
DakshSaini5 <dakshhh98@gmail.com>
2025-08-24 22:11:28 +0530

    first commit

---
+++ project-SignIn/src/App.jsx
@@ -0,0 +1,14 @@
+import LoginSignup from "./compo/LoginSign/LoginSignup"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <LoginSignup />
+      </div>
+    </>
+  )
+}
+
+export default App
